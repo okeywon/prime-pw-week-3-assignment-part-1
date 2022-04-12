fc8971aeffb8b0dbe7537d7c4fa270e641cdfe68
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-12 16:24:06 -0500

    4.h. not working, all else complete.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -100,10 +100,19 @@ console.log('Foods are now backwards:', favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+console.log('Now it is a string:', favoriteFoods.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let newArray = {
+    animalArray: ['fish', 'cat', 'bird', 'dog'],
+    favoriteFoods: ['spaghetti', 'pizza', 'tacos', 'ice cream'],
+    zipArray: function() {
+        return this.animalArray[0 - animalArray.length] + this.favoriteFoods[0 - favoriteFoods.length];
+    }
+}
+
+console.log(newArray.zipArray());
