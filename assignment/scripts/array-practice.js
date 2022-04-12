6ca77a94aced7006870467d4c31e386ffaf49468
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-12 14:00:08 -0500

    4.f stretch completed

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -92,11 +92,15 @@ console.log(favoriteFoods);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+favoriteFoods.sort();
+console.log('Foods are now sorted:', favoriteFoods);
+favoriteFoods.reverse();
+console.log('Foods are now backwards:', favoriteFoods);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
